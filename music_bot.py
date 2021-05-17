b487a6746f5cbc08a18e0a0bb53a4e7f30dd52e2
davsc <dav.schneider03@gmail.com>
2021-05-17 02:49:58 +0200

    added spotify support

--- music_bot.py
+++ music_bot.py
@@ -3,12 +3,42 @@ import youtube_dl
 import asyncio
 from requests.models import PreparedRequest
 import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
+import os
 
 
 class Song:
     def __init__(self, title, url):
         self.title = title
         self.url = url
+
+    def get_session(self):
+        with youtube_dl.YoutubeDL({}) as ydl:
+            info = ydl.extract_info(self.url, download=False)
+            return info["formats"][0]["url"]
+
+
+class URL:
+    def __init__(self, url):
+        self.url = url
+
+    def is_valid_url(self):
+        prepared_request = PreparedRequest()
+        try:
+            prepared_request.prepare_url(self.url, None)
+            return True
+        except Exception:
+            return False
+
+    def is_spotify(self):
+        return "spotify.com" in self.url
+
+    def is_yt(self):
+        return "youtube.com" in self.url
+
+    def get_spotify_id(self):
+        return self.url.split("/")[-1]
 
 
 class MusicBot:
@@ -22,6 +52,14 @@ class MusicBot:
         self.music_paused = False
         self.skip = False
         self.is_playing = False
+
+        load_dotenv()
+
+        credentials = SpotifyClientCredentials(
+                                                os.getenv("SPOTIFY_CLIENT_ID"),
+                                                os.getenv("SPOTIFY_CLIENT_SECRET"))
+
+        self.spotify = spotipy.Spotify(auth=credentials.get_access_token())
 
     async def play(self, message, playtop=False):
         if self.voice_client is None:
@@ -39,18 +77,27 @@ class MusicBot:
     async def playtop(self, message):
         self.play(message, playtop=True)
 
-    async def is_url(self, url):
-        prepared_request = PreparedRequest()
+    async def search_yt(self, query):
         try:
-            prepared_request.prepare_url(url, None)
-            return True
-        except Exception:
-            return False
+            with youtube_dl.YoutubeDL({}) as ydl:
+                info = ydl.extract_info(f"ytsearch:{query}",
+                                        download=False)["entries"][0]
+                return Song(info["title"], info["webpage_url"])
 
-    def convert_to_yt(self, url):
-        
-        
+        except Exception as err:
+            self.text_channel.send("Error while searching YouTube: " + str(err))
 
+    async def convert_to_yt_song(self, url: URL):
+                #if "playlist" in url:
+                #    print(self.spotify.playlist_items(ressource_id)["tracks"][0]["track"]["name"])
+                #    return
+        if "track" in url.url:
+            track = self.spotify.track(url.get_spotify_id())
+            return await self.search_yt(track["name"] + " " + track["album"]["artists"][0]["name"])
+        else:
+            raise ValueError("Spotify Link is not a track")
+
+    def get_playlist_tracks(self):
         return
 
     def pause(self, message):
@@ -60,20 +107,18 @@ class MusicBot:
     def resume(self, message):
         self.voice_client.resume()
         self.music_paused = False
-    
+
     async def remove_index(self, message):
 
         try:
             index = int(message.content[7:])
-            song = self.queue.pop(index)
+            song = self.queue.pop(index + 1)
             await self.text_channel.send("Removed " + song.title)
         except Exception as err:
             await self.text_channel.send("Error while removing song: " +
                                          str(err))
-        
-        return
 
-        
+        return
 
     async def connect(self, message):
         self.text_channel = message.channel
@@ -95,32 +140,33 @@ class MusicBot:
         self.playing = None
         self.is_playing = False
 
-    def get_session_from_url(self, song):
+    def get_song_from_yt(self, url):
+        with youtube_dl.YoutubeDL({}) as ydl:
+            info = ydl.extract_info(url, download=False)
+            return Song(info["title"], info["webpage_url"])
+
+    async def add_to_queue(self, link: str, playtop=False, verbose=True):
         try:
-            with youtube_dl.YoutubeDL({}) as ydl:
-                info = ydl.extract_info(song.url, download=False)
-                return info["formats"][0]["url"]
-        except Exception as err:
-            print(err)
+            url = URL(link)
+            song = None
 
-    async def add_to_queue(self, url, playtop=False):
-        try:
-            with youtube_dl.YoutubeDL({}) as ydl:
-                if(await self.is_url(url)):
-                    info = ydl.extract_info(url, download=False)
+            if(url.is_valid_url()):
+                if url.is_yt():
+                    song = self.get_song_from_yt(url)
+                elif url.is_spotify():
+                    song = await self.convert_to_yt_song(url)
                 else:
-                    await self.text_channel.send("**Searching** for: " + url)
-                    info = ydl.extract_info(f"ytsearch:{url}",
-                                            download=False)["entries"][0]
+                    await self.text_channel.send("Unsupported Platform :(")
 
-                if(playtop):
-                    self.queue.insert(0, Song(info["title"],
-                                              info["webpage_url"]))
-                else:
-                    self.queue.append(Song(info["title"], info["webpage_url"]))
+                if song is not None:
+                    if playtop:
+                        self.queue.append(song)
+                    else:
+                        self.queue.insert(0, song)
 
-                await self.text_channel.send("**Added:** " +
-                                             self.queue[-1].title)
+                    if verbose:
+                        await self.text_channel.send("Added: " + song.title)
+
         except Exception as err:
             await self.text_channel.send(
                                         "Error while retrieving" +
@@ -132,7 +178,7 @@ class MusicBot:
 
         queue_str = f"**Now Playing: ** {self.playing.title} \n**Queue: \n **"
         for i in range(0, len(self.queue)):
-            queue_str += f"{i}. ```{self.queue[i].title}``` \n"
+            queue_str += f"{i + 1}. ```{self.queue[i].title}``` \n"
 
         await self.text_channel.send(queue_str)
 
@@ -152,7 +198,7 @@ class MusicBot:
         self.is_playing = True
         while(len(self.queue) != 0):
             self.playing = self.queue.pop(0)
-            session = self.get_session_from_url(self.playing)
+            session = self.playing.get_session()
             source = discord.FFmpegPCMAudio(
                             source=session,
                             before_options="-reconnect 1 " +
